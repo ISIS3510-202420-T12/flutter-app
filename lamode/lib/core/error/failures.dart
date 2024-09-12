06f38bd3e994b9c6bd420d60ecbefadd29a9d0e5
JackRakham <j.villatet@uniandes.edu.co>
2024-09-12 11:57:08 -0500

    Basic skeleton adjust

---
+++ lamode/lib/core/error/failures.dart
@@ -0,0 +1,9 @@
+abstract class Failure {
+
+}
+
+
+class ServerFailure extends Failure {}
+
+class LocalFailure extends Failure {}
+
--- lamode/lib/main.dart
+++ lamode/lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+void main() {
+  runApp(const MainApp());
+}
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
 }
