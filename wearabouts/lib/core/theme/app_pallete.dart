e82162388dac110520a713433a5d4ca8e3bea615
JackRakham <j.villatet@uniandes.edu.co>
2024-09-24 19:41:00 -0500

    Folder structure

---
+++ wearabouts/lib/core/theme/app_pallete.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+class Palllete {
+
+
+
+  static Color backgroundColor1 = Color.fromRGBO(255, 255, 255, 1);
+  static Color backgroundColor2 = Color(0xFFADCEFF);
+
+  static Color whiteColor = Color(0xFFFFFEFE);
+  static Color blueColor1 = Color.fromRGBO(50, 173, 255, 1);
+
+}
