647002711caaaab2e9612246724e42bfa3336bb4
JackRakham <j.villatet@uniandes.edu.co>
2024-09-28 21:06:52 -0500

    Arreglo pantalla FirstTime

--- wearabouts/lib/features/home/view/pages/homePage.dart
+++ wearabouts/lib/features/home/view/pages/homePage.dart
@@ -1,10 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:wearabouts/features/home/view/widgets/appBar.dart';
+import 'package:wearabouts/features/home/view/widgets/categoryTab.dart';
+
+import '../../../../core/theme/app_pallete.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        appBar: const MyAppBar(),
+        drawer: Drawer(
+            child: ListView(
+          children: [
+            DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Pallete.color1,
+                ),
+                child: Text("Options",
+                    style: TextStyle(color: Pallete.whiteColor, fontSize: 24))),
+            ListTile(
+              title: const Text("My profile"),
+              onTap: () {
+                // Handle tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              title: const Text("My donations"),
+              onTap: () {
+                // Handle tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              title: const Text("Settings"),
+              onTap: () {
+                // Handle tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              title: const Text("About us"),
+              onTap: () {
+                // Handle tap
+                Navigator.pop(context); // Close the drawer
+              },
+            )
+          ],
+        )),
+        body: Center(
+          child: Column(
+            children: [
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient:
+                      LinearGradient(colors: [Pallete.color3, Pallete.color2]),
+                ),
+                child: const SizedBox(height: 10, width: double.infinity),
+              ),
+              const SizedBox(height: 10),
+              const Text("Marketplace",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10),
+              const Row(
+                children: [
+                  CategoryTab(
+                    name: "Shoes",
+                    assetName: "Assets/shoes.svg",
+                  ),
+                  CategoryTab(
+                    name: "Bottoms",
+                    assetName: "Assets/shoes.svg",
+                  ),
+                  CategoryTab(
+                    name: "Tops",
+                    assetName: "Assets/shoes.svg",
+                  ),
+                  CategoryTab(
+                    name: "Jackets",
+                    assetName: "Assets/jackets.svg",
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
---
+++ wearabouts/lib/features/home/view/widgets/appBar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({
+    super.key,
+  });
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text("WearAbouts",
+          style: TextStyle(fontWeight: FontWeight.bold)),
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      actions: <Widget>[
+        IconButton(
+            onPressed: () {}, icon: const Icon(Icons.leave_bags_at_home)),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+      ],
+      leading: Row(
+        children: [
+          IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: const Icon(Icons.menu)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
+        ],
+      ),
+    );
+  }
+}
