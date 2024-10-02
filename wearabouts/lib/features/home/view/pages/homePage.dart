f281eced1318832648f06ebbf4382813ee3008e9
Daniel <69821353+DanielAcevedo99@users.noreply.github.com>
2024-10-02 13:02:21 -0500

    .

--- wearabouts/lib/features/home/view/pages/homePage.dart
+++ wearabouts/lib/features/home/view/pages/homePage.dart
@@ -28,128 +28,121 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const MyAppBar(),
-      drawer: Drawer(
-        child: ListView(
+        appBar: const MyAppBar(),
+        drawer: Drawer(
+            child: ListView(
           children: [
             DrawerHeader(
-              decoration: BoxDecoration(
-                color: Pallete.color1,
-              ),
-              child: Text(
-                "Options",
-                style: TextStyle(
-                  color: Pallete.whiteColor,
-                  fontSize: 24,
+                decoration: BoxDecoration(
+                  color: Pallete.color1,
                 ),
-              ),
-            ),
+                child: Text("Options",
+                    style: TextStyle(color: Pallete.whiteColor, fontSize: 24))),
             ListTile(
               title: const Text("My profile"),
               onTap: () {
-                Navigator.pop(context);
+                // Handle tap
+                Navigator.pop(context); // Close the drawer
               },
             ),
             ListTile(
               title: const Text("My donations"),
               onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/donations');
+                // Handle tap
+                Navigator.pop(context); // Close the drawer
               },
             ),
             ListTile(
               title: const Text("Settings"),
               onTap: () {
-                Navigator.pop(context);
+                // Handle tap
+                Navigator.pop(context); // Close the drawer
               },
             ),
             ListTile(
               title: const Text("About us"),
               onTap: () {
-                Navigator.pop(context);
+                // Handle tap
+                Navigator.pop(context); // Close the drawer
               },
             )
           ],
-        ),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            DecoratedBox(
-              decoration: BoxDecoration(
-                gradient:
-                    LinearGradient(colors: [Pallete.color3, Pallete.color2]),
+        )),
+        body: Center(
+          child: Column(
+            children: [
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient:
+                      LinearGradient(colors: [Pallete.color3, Pallete.color2]),
+                ),
+                child: const SizedBox(height: 10, width: double.infinity),
               ),
-              child: const SizedBox(height: 10, width: double.infinity),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              "Marketplace",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            const Padding(
-              padding: EdgeInsets.all(12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CategoryTab(
-                    name: "Shoes",
-                    assetName: "Assets/shoes.svg",
-                  ),
-                  CategoryTab(
-                    name: "Bottoms",
-                    assetName: "Assets/bottoms.svg",
-                  ),
-                  CategoryTab(
-                    name: "Tops",
-                    assetName: "Assets/tops.svg",
-                  ),
-                  CategoryTab(
-                    name: "Jackets",
-                    assetName: "Assets/jackets.svg",
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Image.network(
-                "https://www.fashiongonerogue.com/wp-content/uploads/2021/04/Model-Chic-Fashion.jpg",
-                fit: BoxFit.fill,
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [Text("New releases"), Text("See all")],
-              ),
-            ),
-            const Divider(
-              color: Colors.black,
-            ),
-            //--------------- CARDS -------------------------
-            Consumer<MarketPlaceViewModel>(
-              builder: (context, viewMoodel, child) {
-                return Container(
-                  color: const Color.fromRGBO(73, 13, 8, 1),
-                  child: Expanded(
-                    child: Column(
-                      children: viewMoodel.items.map((clothe) {
-                        return SizedBox(
-                          height: 10,
-                          child: ClothesCard(item: clothe),
-                        );
-                      }).toList(),
+              const SizedBox(height: 10),
+              const Text("Marketplace",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10),
+              const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CategoryTab(
+                      name: "Shoes",
+                      assetName: "Assets/shoes.svg",
                     ),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+                    CategoryTab(
+                      name: "Bottoms",
+                      assetName: "Assets/bottoms.svg",
+                    ),
+                    CategoryTab(
+                      name: "Tops",
+                      assetName: "Assets/tops.svg",
+                    ),
+                    CategoryTab(
+                      name: "Jackets",
+                      assetName: "Assets/jackets.svg",
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Image.network(
+                  "https://www.fashiongonerogue.com/wp-content/uploads/2021/04/Model-Chic-Fashion.jpg",
+                  fit: BoxFit.fill,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("New releases"), Text("See all")],
+                ),
+              ),
+              const Divider(
+                color: Colors.black,
+              ),
+              //--------------- CARDS -------------------------
+              Consumer<MarketPlaceViewModel>(
+                builder: (context, viewMoodel, child) {
+                  return Container(
+                    color: const Color.fromRGBO(73, 13, 8, 1),
+                    child: Expanded(
+                      child: Column(
+                        children: viewMoodel.items.map((clothe) {
+                          return SizedBox(
+                            height: 10,
+                            child: ClothesCard(item: clothe),
+                          ); // Tu widget para cada elemento
+                        }).toList(),
+                      ),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+        ));
   }
 }
--- wearabouts/lib/main.dart
+++ wearabouts/lib/main.dart
@@ -3,8 +3,6 @@ import 'package:wearabouts/core/theme/theme.dart';
 import 'package:wearabouts/features/auth/view/pages/firstTimePage.dart';
 import 'package:provider/provider.dart';
 import 'package:wearabouts/features/home/viewmodel/marketPlaceViewModel.dart';
-import 'package:wearabouts/features/donation/pages/donationPage.dart';
-import 'package:wearabouts/features/home/view/pages/homePage.dart';
 
 void main() {
   runApp(MultiProvider(providers: [
@@ -18,14 +16,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "WearAbouts",
-      theme: AppTheme.lightThemeMode,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const FirstTimePage(), // Página de inicio
-        '/home': (context) => const HomePage(), // Página principal (HomePage)
-        '/donations': (context) => const DonationPage(), // Página de donaciones
-      },
-    );
+        title: "WearAbouts",
+        theme: AppTheme.lightThemeMode,
+        home: const FirstTimePage());
   }
 }
