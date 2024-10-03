648c4edd7561986d7fb662e22a92f336b5abebd3
JackRakham <j.villatet@uniandes.edu.co>
2024-10-02 19:58:56 -0500

    First version bottombar + navigation + FaIcons

--- wearabouts/lib/features/home/view/widgets/clothesCard.dart
+++ wearabouts/lib/features/home/view/widgets/clothesCard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wearabouts/core/theme/app_pallete.dart';
 import 'package:wearabouts/features/home/model/clothe.dart';
 
 class ClothesCard extends StatefulWidget {
@@ -16,6 +17,28 @@ class _ClothesCardState extends State<ClothesCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Text("HEyyy");
+    return Card(
+        elevation: 5.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: Pallete.color2,
+                child: Image.network(item.imagesURLs[0],
+                    height: 120, width: 180, fit: BoxFit.fitHeight),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              item.title,
+              style: const TextStyle(fontSize: 14),
+            ),
+            const SizedBox(height: 100),
+            Text("1000")
+          ],
+        ));
   }
 }
