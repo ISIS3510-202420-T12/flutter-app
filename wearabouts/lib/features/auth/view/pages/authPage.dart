1cfbeea4d483acbc610cfd092753c3bda5a31409
Laura <l.ariasf@uniandes.edu.co>
2024-10-02 23:17:47 -0500

    Biometric Authentication + Log in screen (currently hidden)

--- wearabouts/lib/features/auth/view/pages/authPage.dart
+++ wearabouts/lib/features/auth/view/pages/authPage.dart
@@ -1,35 +1,154 @@
 import 'package:flutter/material.dart';
 import 'package:wearabouts/features/home/view/pages/homePage.dart';
-
 import '../../../../core/theme/app_pallete.dart';
 
-class Authpage extends StatelessWidget {
-  const Authpage({super.key});
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text("This will be the auth page",
-            style: TextStyle(fontSize: 50)),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomePage()),
-              );
-            },
-            style: ElevatedButton.styleFrom(
-                fixedSize: const Size(350, 70),
-                backgroundColor: Pallete.color2,
-                shadowColor: Pallete.transparentColor),
-            child: const Text(
-              "Home page button",
-              style: TextStyle(fontSize: 24, color: Colors.white),
-            ))
-      ],
-    ));
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(40.0,20.0,40.0,20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.lock,
+              size: 80,
+              color: Colors.black,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Welcome to\nWearAbouts",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 40),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,  // Aligns children to the start (left) of the column
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 2, bottom: 5),  // Adds some padding to align with the TextField
+                  child: Text(
+                    "Username",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide.none,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Padding(
+                  padding: EdgeInsets.only(left: 2, bottom: 5),  // Adds some padding to align with the TextField
+                  child: Text(
+                    "Password",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide.none,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Forgot password?',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Pallete.color3, Pallete.color2], // Your gradient colors
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+                borderRadius: BorderRadius.circular(50), // Same border radius as button
+              ),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(150, 50),
+                  backgroundColor: Colors.transparent, // Transparent background to see the gradient
+                  shadowColor: Colors.transparent, // To avoid shadow overlapping gradient
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+                child: Text(
+                  "Log in",
+                  style: TextStyle(fontSize: 25, color: Colors.white.withOpacity(0.6)),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+            const Text(
+              'or continue with',
+              style: TextStyle(color: Colors.grey),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(50, 50),
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: const BorderSide(color: Colors.grey),
+                ),
+              ),
+              child: Image.network(
+                'https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg',
+                height: 24,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- wearabouts/lib/features/auth/view/pages/firstTimePage.dart
+++ wearabouts/lib/features/auth/view/pages/firstTimePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart'; // Import local_auth
 import 'package:wearabouts/core/theme/app_pallete.dart';
-import 'package:wearabouts/features/auth/view/pages/authPage.dart';
+import 'package:wearabouts/features/home/view/pages/homePage.dart';
 import 'package:wearabouts/features/auth/view/widgets/discoverNowButton.dart';
 
 class FirstTimePage extends StatefulWidget {
@@ -11,6 +12,32 @@ class FirstTimePage extends StatefulWidget {
 }
 
 class _FirstTimePageState extends State<FirstTimePage> {
+  final LocalAuthentication auth = LocalAuthentication();
+
+  Future<void> _authenticate() async {
+    bool authenticated = false;
+    try {
+      print('Attempting to authenticate...'); // Debug print
+      authenticated = await auth.authenticate(
+        localizedReason: 'Please authenticate to continue',
+        options: const AuthenticationOptions(
+          stickyAuth: true,
+          biometricOnly: true,
+        ),
+      );
+      print('Authenticated: $authenticated'); // Debug print
+      if (authenticated) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const HomePage()),
+        );
+      }
+    } catch (e) {
+      // Handle error here
+      print('Error during authentication: $e'); // Debug print
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,60 +48,88 @@ class _FirstTimePageState extends State<FirstTimePage> {
             height: double.maxFinite,
             child: DecoratedBox(
               decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                        'Assets/homebackground.jpeg'), // Your image file path
-                    fit: BoxFit
-                        .cover, // Ensures the image covers the entire background
-                  ),
-                  gradient: LinearGradient(
-                    colors: [
-                      Colors.transparent, // Starting color
-                      Colors.black, // Ending color
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  )),
+                image: DecorationImage(
+                  image: AssetImage('Assets/homebackground.jpeg'),
+                  fit: BoxFit.cover,
+                ),
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.transparent,
+                    Colors.black,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
             ),
           ),
           Container(
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 colors: [
-                  Colors.transparent, // Start color
-                  Colors.black.withOpacity(0.7), // End color
+                  Colors.transparent,
+                  Colors.black.withOpacity(0.7),
                 ],
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
               ),
             ),
             child: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                  const Text("Discover your image",
-                      style: TextStyle(
-                          fontSize: 40,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Discover your image",
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   const SizedBox(height: 40),
                   DiscoverNowButton(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Authpage()),
-                      );
-                    },
+                    onTap: _authenticate, // Call _authenticate on tap
                   ),
                   const SizedBox(
                     height: 100,
-                  )
-                ])),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+class DiscoverNowButton extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const DiscoverNowButton({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [Pallete.color3, Pallete.color2]),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          fixedSize: const Size(350, 70),
+          backgroundColor: Pallete.transparentColor,
+          shadowColor: Pallete.transparentColor,
+        ),
+        child: const Text(
+          "Discover now",
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
+
+
